cb4098b5d6392a1658780f7b5bab8cde461b1bff
Dabaridu <40957416+Dabaridu@users.noreply.github.com>
2023-05-03 01:19:22 +0200

    Added a driver for stepper motor

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,15 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include "PinChangeInterrupt.h"
+#include "A4988.h"
 
 //constants are here 
 #define LCDcolums 2
 #define LCDrows 16
 
 //rotary encoder
-#define CLK 2
-#define DT 3
+#define CLK 2 //2
+#define DT 3 //3
 #define SW 9
 
 //Variables are here 
@@ -26,19 +27,34 @@ unsigned long debounceDelay = 10; //rotary encoder button
 
 int simpleinterval;
 
+bool StartFlow; //variable to control the flow of the start of the program
+int Navojidolzina;
+int Navojisirina;
+int precnipomikstopinj = 90;
+
 bool Clicked = false;
 
 //Classes are here
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 //leave clear
 
+// using a 200-step motor (most common)
+#define MOTOR_STEPS 200
+// configure the pins connected
+#define DIR 4
+#define STEP 7
+#define MS1 10
+#define MS2 11
+#define MS3 12
+A4988 stepper(MOTOR_STEPS, DIR, STEP);
+
 class Screen {
   private:
     String Label;
-    float Value;
     float increment;
   
   public:
+    float Value;
     Screen(String Label, float Value, float increment){
       this -> Label = Label;
       this -> Value = Value;
@@ -62,14 +78,14 @@ class Screen {
 };
 
 //making it public so that all functions can see it 
-  Screen screens[] = {
-    Screen("Start  ", 0, 1),
-    Screen("Dolzina", 10, 0.1),
-    Screen("Sirina ", 5, 0.1),
-    Screen("Navoji ", 10, 0.1),
-    Screen("Calq   ", 0, 0.1),
+  Screen *screens[] = {
+    new Screen("Start  ", 0, 1),
+    new Screen("Dolzina", 15, 0.1),
+    new Screen("Sirina ", 3, 0.1),
+    new Screen("Navoji ", 10, 0.1),
+    new Screen("Calq   ", 0, 0.1),
   };
-int menusize = 4;
+int menusize = 4; //0<->4
 
 
 void button_press(){
@@ -99,7 +115,7 @@ void encoder_value() {
         }
       }
       else {
-        screens[counter].sub();
+        screens[counter]->sub();
       }
     } else {
       if (Clicked == true){
@@ -108,7 +124,7 @@ void encoder_value() {
         }
       }
       else{
-        screens[counter].add();
+        screens[counter]->add();
       }
     }
     // print the value in the serial monitor window
@@ -138,6 +154,9 @@ void setup()
   attachInterrupt(0, encoder_value, CHANGE);
   attachInterrupt(1, encoder_value, CHANGE);
   attachPCINT(digitalPinToPCINT(SW), button_press, CHANGE);
+
+  // Set target motor RPM to 1RPM and microstepping to 1 (full step mode)
+  stepper.begin(15, 1);
 }
 
 void select(){
@@ -154,12 +173,27 @@ void select(){
   }
 }
 
+void exenavijanje(){
+  for (Navojisirina = screens[2]->Value; Navojisirina > 0; Navojisirina -= 1){
+    for (Navojidolzina = screens[1]->Value; Navojidolzina > 0; Navojidolzina -= 1){
+      //stepper.rotate(360); //rotacijski 
+      stepper.rotate(precnipomikstopinj); //prečni
+    }
+    precnipomikstopinj = ~(precnipomikstopinj-1); //eniški kompliment, -1 --> nasprotna vrednost (pomik prečne osi v drugo smer kot prej)
+  }
+  screens[0]->Value = 0;
+}
+
 void loop(){
 //    screens[counter].draw();
-if (millis() - simpleinterval > 1500){
-  lcd.clear();
-}
+  if (millis() - simpleinterval > 1500){
+    lcd.clear();
+  }
 simpleinterval = millis();
-screens[counter].draw();
+screens[counter]->draw();
 select();
+
+  if((screens[0]->Value)>0){ //if start is more than 0
+      exenavijanje();
+    }
 }
